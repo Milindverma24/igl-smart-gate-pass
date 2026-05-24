da30af2d1bcf2c4878eed7787827c9ee8be43601
Milindverma24 <anshverma24112005@gmail.com>
2026-05-25 04:32:07 +0530

    Removed pyttsx3 for Streamlit deployment

--- utils/voice_alert.py
+++ utils/voice_alert.py
@@ -1,32 +1,3 @@
-import pyttsx3
-
-# =====================================================
-# SPEAK ALERT
-# =====================================================
-
 def speak_alert(message):
 
-    engine = pyttsx3.init()
-
-    engine.setProperty(
-        'rate',
-        150
-    )
-
-    engine.setProperty(
-        'volume',
-        1
-    )
-
-    voices = engine.getProperty(
-        'voices'
-    )
-
-    engine.setProperty(
-        'voice',
-        voices[0].id
-    )
-
-    engine.say(message)
-
-    engine.runAndWait()
+    print(message)
